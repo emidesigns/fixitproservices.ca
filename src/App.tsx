19f385f995f253eefeefc42bca27bbe986428abd
Emilio <ediazdesigns@gmail.com>
2024-01-04 14:18:17 -0500

    adding categories for handyman work

--- src/App.tsx
+++ src/App.tsx
@@ -19,27 +19,29 @@ function App() {
       <section className="text-white">
         <div className="grid text-center">
           <h2 className="max-w-3xl mx-auto px-10 py-14 sm:pt-6 sm:py-18 font-extrabold">
-            Turn your house into a dream home with our
-            handyman team. From small repairs to home improvements,
-            we’ve got you covered.{" "}
+            Turn your house into a dream home with our handyman team. From small
+            repairs to home improvements, we’ve got you covered.{" "}
           </h2>
           <div className="max-w-full bg-white py-10">
             <p className="max-w-3xl px-8 sm:px-0 mx-auto text-black text-lg">
-            In every interaction, we strive to make a lasting impact through top-notch <strong>handyman</strong> and <strong>repair services</strong> in{" "}
-              <strong>Toronto</strong> and <strong>Etobicoke</strong>. Our commitment goes beyond transactions, fostering enduring customer relationships built on <strong>trust</strong>,{" "}
-              <strong>reliability</strong>, and{" "}
+              In every interaction, we strive to make a lasting impact through
+              top-notch <strong>handyman</strong> and{" "}
+              <strong>repair services</strong> in <strong>Toronto</strong> and{" "}
+              <strong>Etobicoke</strong>. Our commitment goes beyond
+              transactions, fostering enduring customer relationships built on{" "}
+              <strong>trust</strong>, <strong>reliability</strong>, and{" "}
               <strong>unparalleled service</strong>.
             </p>
           </div>
         </div>
       </section>
 
-      <section className="bg-gray-900 max-w-full">
-        <h3 className="text-center text-yellow py-12 px-8 sm:px-0">
+      <section className="bg-gray-900 max-w-full pb-8">
+        <h3 className="text-center text-yellow py-12 px-8">
           Some of the services we offer:
         </h3>
 
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 max-w-6xl mx-auto mb-8">
           <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
             <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
               Drywall
@@ -81,6 +83,52 @@ function App() {
               With precision and attention to detail, we excel in TV and
               furniture mounting, as well as expert furniture assembly ensuring
               both functionality and aesthetic appeal in your living spaces.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 max-w-6xl mx-auto">
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
+            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
+              Painting
+            </h4>
+            <p>
+              Revitalize your space with our expert painting services, from
+              priming to perfect sheens on walls and ceilings. Transform your
+              environment effortlessly.
+            </p>
+          </div>
+
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
+            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
+              Appliances
+            </h4>
+            <p>
+              Code-compliant minor appliance installations. We follow
+              manufacturers' instructions meticulously, guaranteeing optimal
+              functionality for your appliances.
+            </p>
+          </div>
+
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
+            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
+              Hardscaping
+            </h4>
+            <p>
+              Elevate your outdoor space with our hardscaping services. We
+              specialize in precise subfloor preparation, effective drainage,
+              and meticulous leveling, ensuring lasting retention of interlock,
+              pavers, and more for a stunning and durable landscape.
+            </p>
+          </div>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
+            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
+              Electrical
+            </h4>
+            <p>
+              Upgrade your space effortlessly with our small electrical
+              projects. From light fixtures to outlet replacements, we ensure
+              expert solutions for a safer and more functional home.
             </p>
           </div>
         </div>
