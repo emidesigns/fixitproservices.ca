955b034f051d8b2bb6267fa02fe0d87641b6651a
Emilio <ediazdesigns@gmail.com>
2024-02-05 22:10:07 -0500

    fixing contact form background

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,12 @@ function App() {
           </div>
         </section>
         <section>
-        <div className="grid text-center">
-          <h2 className="max-w-3xl mx-auto text-xl md:text-3xl px-10 pb-20 sm:pt-6 font-medium text-white">
-            Turn your house into a dream home with our handyman team. From small
-            repairs to home improvements, we’ve got you covered.{" "}
-          </h2>
-        </div>
+          <div className="grid text-center">
+            <h2 className="max-w-3xl mx-auto text-xl md:text-3xl px-10 pb-20 sm:pt-6 font-medium text-white">
+              Turn your house into a dream home with our handyman team. From
+              small repairs to home improvements, we’ve got you covered.{" "}
+            </h2>
+          </div>
         </section>
       </div>
 
@@ -51,26 +51,20 @@ function App() {
       </section>
 
       <section className="bg-gray-900 max-w-full pb-8">
-        <h3 className="text-center text-yellow py-12 px-8">
-          Some of the services we offer:
-        </h3>
+        <h3 className="text-center text-yellow py-12 px-8">Our services:</h3>
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 max-w-6xl mx-auto mb-8">
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Drywall
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Drywall</h4>
             <p>
-              We specialize in drywall work, including hanging, taping, priming,
-              and painting to ensure smooth and seamless surfaces transforming
-              raw drywall into flawlessly finished spaces.
+              Hanging, taping, priming, and painting to ensure smooth and
+              seamless surfaces transforming raw drywall into flawlessly
+              finished spaces.
             </p>
           </div>
 
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Carpentry
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Carpentry</h4>
             <p>
               With expertise in trimming, framing, and small carpentry, we bring
               precision and craftsmanship to every project from framing
@@ -78,10 +72,8 @@ function App() {
             </p>
           </div>
 
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Flooring
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Flooring</h4>
             <p>
               Specializing in vinyl and laminate flooring installations. From
               precise measurements to seamless placement, we bring a skilled
@@ -89,10 +81,8 @@ function App() {
               aesthetically pleasing flooring solutions.
             </p>
           </div>
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Mounting & Assembly
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Mounting & Assembly</h4>
             <p>
               With precision and attention to detail, we excel in TV and
               furniture mounting, as well as expert furniture assembly ensuring
@@ -102,10 +92,8 @@ function App() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 max-w-6xl mx-auto">
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Painting
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Painting</h4>
             <p>
               Revitalize your space with our expert painting services, from
               priming to perfect sheens on walls and ceilings. Transform your
@@ -113,10 +101,8 @@ function App() {
             </p>
           </div>
 
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Appliances
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Appliances</h4>
             <p>
               Code-compliant minor appliance installations. We follow
               manufacturers' instructions meticulously, guaranteeing optimal
@@ -124,10 +110,8 @@ function App() {
             </p>
           </div>
 
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Hardscaping
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Hardscaping</h4>
             <p>
               Elevate your outdoor space with our hardscaping services. We
               specialize in precise subfloor preparation, effective drainage,
@@ -135,10 +119,8 @@ function App() {
               pavers, and more for a stunning and durable landscape.
             </p>
           </div>
-          <div className="bg-white py-4 px-8 sm:px-6 shadow-md">
-            <h4 className="text-2xl sm:text-3xl font-bold mb-4 text-center">
-              Electrical
-            </h4>
+          <div className="bg-white py-4 px-8 sm:px-6 shadow-md text-center">
+            <h4 className="text-2xl font-medium mb-4">Electrical</h4>
             <p>
               Upgrade your space effortlessly with our small electrical
               projects. From light fixtures to outlet replacements, we ensure
@@ -148,12 +130,7 @@ function App() {
         </div>
       </section>
 
-      <section className="grid py-20">
-        <h1 className="sm:text-7xl md:text-8xl py-8 mx-auto text-white">
-          Get in touch!
-        </h1>
-        <Contact />
-      </section>
+      <Contact />
       <Footer />
       <PoweredBy />
     </>
