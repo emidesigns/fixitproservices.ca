814a09e393e6538de940cff8624204761cda28e7
Emilio <ediazdesigns@gmail.com>
2025-10-16 17:01:51 -0400

    code cleanup and bg image issue fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import Footer from "./Footer";
 import "./index.css";
-import ScrollToTop from "./ScrollToTop.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
